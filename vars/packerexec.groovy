970842f515b8d3b8ce57625bd668fdb39d3a7c6c
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-01 15:24:51 +0530

    Update packerexec.groovy

--- vars/packerexec.groovy
+++ vars/packerexec.groovy
@@ -1,5 +1,5 @@
 def call(String step=''){
   sh 'packer validate /var/lib/jenkins/workspace/sas2/groupwork/resources/pack.json'
-  sh 'packer build -machine-readable -var 'aws_access_key="${AWS_ACCESS_KEY_ID}"'  -var 'aws_secret_key="${AWS_SECRET_ACCESS_KEY}"' pack.json'
+  sh 'packer build -machine-readable \ -var 'aws_access_key="${AWS_ACCESS_KEY_ID}"' \ -var 'aws_secret_key="${AWS_SECRET_ACCESS_KEY}"' \ pack.json'
   echo "step :${step}"
   }
