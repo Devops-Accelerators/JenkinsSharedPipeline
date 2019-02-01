0bb324b66f7ace2336492f603c8e10f25c1bc71f
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-01 13:46:30 +0530

    Update packerexec.groovy

--- vars/packerexec.groovy
+++ vars/packerexec.groovy
@@ -1,4 +1,6 @@
 def call(String step=''){
-  sh ' ./var/lib/jenkins/workspace/sas2/groupwork/scriptPack.sh'
-  echo" step :${step}"
+  sh 'packer validate /var/lib/jenkins/workspace/sas2/groupwork/resources/pack.json'
+  sh 'AMI=`packer build -machine-readable -var 'aws_access_key=${AWS_ACCESS_KEY_ID}'  -var 'aws_secret_key=${AWS_SECRET_KEY}' pack.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'` 
+echo 'variable "AMI_ID" { type = "map" default = "'${AMI_ID}'" }' > /var/lib/jenkins/workspace/sas2/groupwork/resources/amivar.tf
+  echo "step :${step}"
   }
