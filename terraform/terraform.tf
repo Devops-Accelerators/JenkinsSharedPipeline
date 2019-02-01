5b34ff409c381cf113128b64039ba2ae30c4bd33
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-01 13:25:04 +0530

    Create terraform.tf

---
+++ terraform/terraform.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+          region ="eu-west-1"
+        }
+        resource "aws_instance" "inst" {
+          ami = "${AMI_ID}"
+          instance_type = "t2.micro"
+
+        tags{
+                Name="my_instance1"
+        }
+  }
