5d8cfaecca034682aacba0f45c28b4c676c1e546
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-05 16:53:37 +0530

    Update packerexec.groovy

--- vars/packerexec.groovy
+++ vars/packerexec.groovy
@@ -1,13 +1,14 @@
 def call(String step=''){
   sh 'packer validate /var/lib/jenkins/workspace/sas2/resources/pack.json'
   sh 'packer build -debug -machine-readable -var aws_access_key="${AWS_ACCESS_KEY_ID}"  -var aws_secret_key="${AWS_SECRET_ACCESS_KEY}"   /var/lib/jenkins/workspace/sas2/resources/pack.json > ~/workspace/sas2/terraform/output.txt '
-  sh 'AMI_ID=\'"$(awk \'END {print $NF}\' ~/workspace/sas2/terraform/output.txt)"\'' 
+  sh 'AMI_ID=$(awk \'END {print $NF}\' ~/workspace/sas2/terraform/output.txt)' 
+  //sh 'AMI_ID=$AMIID'
   sh 'pwd'
-  sh 'temp="variable \"AMI_ID\" { default = "'
+  sh 'temp=\'variable "AMI_ID" { default = " \''
   sh 'pwd'
-  sh 'temp2="}"'
+  sh 'temp2=\'"}\''
   sh 'pwd'
-  sh 'c="${temp}${AMI_ID}${temp2}"'
+  sh 'c="$temp$AMI_ID$temp2"'
   sh 'pwd'
   echo "$c "  //> ~/workspace/sas2/terraform/var.tf"
   echo "step :${step}"
