14b90dee99558935c0d2660e6dd037751c9db11f
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-07 16:33:04 +0530

    Update var.tf

--- terraform/var.tf
+++ terraform/var.tf
@@ -2,9 +2,9 @@
   default = "t2.micro"
 }
 
-variable "key_path" {
-  default = "{{env `SAS_PUB`}}"
-}
+#variable "key_path" {
+ # default = "{{env `SAS_PUB`}}"
+#}
 variable "key_name" {
   default = "SAS_PRI..ppk"
 }
