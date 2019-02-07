96f6fe6c43363e6f2ca2de3c0ebb0ce21ec32867
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-07 14:25:11 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,11 +1,17 @@
 provider "aws" {
           region ="eu-west-1"
         }
-        resource "aws_instance" "inst" {
-          ami = "${data.aws_ami.image.id}"
-          instance_type = "t2.micro"
-
+resource "aws_key_pair" "sas"{
+          key_name = "${var.key_name}" 
+          public_key = "${file("${var.key_path}")}"
+        }
+resource "aws_instance" "inst" {
+          ami ="${var.ami}"
+          instance_type = "t2.micro" "${var.instance_type}"
+          key_name = "${aws_key_pair.sas.id}"
         tags{
                 Name="my_instance1"
-        }
-  }
+            }
+       }
+       
+
