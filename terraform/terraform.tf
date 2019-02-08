7909a5bd56779ec7ee9328ec9b3045288738d7e2
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-08 09:42:22 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,14 +1,14 @@
 provider "aws" {
           region ="eu-west-1"
         }
-resource "aws_key_pair" "mykey"{
+resource "aws_key_pair" "default"{
   key_name = "${var.key_name}" 
   public_key = "${file("${var.key_path}")}"
 }
 resource "aws_instance" "inst" {
           ami ="${data.aws_ami.image.id}"
           instance_type ="${var.instance_type}"
-          key_name = "${aws_key_pair.mykey.id}"
+          key_name = "${aws_key_pair.default.id}"
         tags{
                 Name="my_instance1"
             }
