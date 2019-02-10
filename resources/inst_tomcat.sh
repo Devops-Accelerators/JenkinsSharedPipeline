5e2a5e4f832871064d23d39ef37de1459875fe6b
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-10 18:44:40 +0530

    Create inst_tomcat.sh

---
+++ resources/inst_tomcat.sh
@@ -0,0 +1,5 @@
+sudo apt-get update && apt-get upgrade
+sudo apt-get install tomcat8
+sudo apt-get install tomcat8-docs tomcat8-examples tomcat8-admin
+sudo systemctl start tomcat8
+echo "tomcat installed successfully..............."
