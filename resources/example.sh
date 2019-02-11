6a865a17065324a5c10589b6cc76bdd7cd24fb23
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 13:59:26 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -2,8 +2,6 @@ echo "hello"
 export LANGUAGE="en_US.UTF-8"
 echo 'LANGUAGE="en_US.UTF-8"' >> /etc/default/locale
 echo 'LC_ALL="en_US.UTF-8"' >> /etc/default/locale
-locale -a
-locale
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo systemctl start nginx
