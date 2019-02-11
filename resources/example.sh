c8bddd840ffe5bb56c9fd38689fe3e239ab45e84
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 23:48:35 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -6,10 +6,14 @@ sudo apt-get update -y
 #sudo dpkg-reconfigure locales
 sudo locale-gen de_DE.UTF-8 en_US.UTF-8
 sudo apt-get install language-pack-en -y
-export LANGUAGE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LANG=en_US.UTF-8
-export LC_TYPE=en_US.UTF-8
+#export LANGUAGE=en_US.UTF-8
+#export LC_ALL=en_US.UTF-8
+#export LANG=en_US.UTF-8
+#export LC_TYPE=en_US.UTF-8
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+locale-gen en_US.UTF-8
 sudo locale-gen
 sudo dpkg-reconfigure locales
 sudo apt-get install nginx -y
