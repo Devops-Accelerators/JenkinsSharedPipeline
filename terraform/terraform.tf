710fef75e1bcdb76db56125aad28455e98b521c3
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 11:11:41 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -8,6 +8,22 @@ resource "aws_instance" "inst" {
         tags{
                 Name="my_instance"
             }
+resource "aws_volume_attachment" "vol_att" {
+  device_name = "/dev/sdh"
+  volume_id   = "${aws_ebs_volume.vol.id}"
+  instance_id = "${aws_instance.inst.id}"
+}
+ resource "aws_ebs_volume" "vol" {
+ size              = 1
+ availability_zone = "eu-west-1a"
+ tags = {
+    Name = "ebs_volume"
+  }
+
+}
+
+
+          
        }
        
 
