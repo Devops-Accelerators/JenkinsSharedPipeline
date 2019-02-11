4a31e46f15203b4b108800c20047695263db80ea
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 11:18:02 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -1,5 +1,6 @@
 echo "hello"
-sudo locale-gen en_US.UTF-8
+sudo locale-gen de_DE.UTF-8 en_US.UTF-8
+sudo apt-get install language-pack-en -y
 sudo apt-get update -y
 sudo apt-get install nginx -y
 sudo apt-get install ufw
