23a553d2ed9c118a8f735c253c8899d9bed18379
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 15:25:41 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -1,6 +1,7 @@
 echo "hello"
 sudo apt-get update -y
 sudo locale-gen en_US en_US.UTF-8
+sudo apt-get install language-pack-en
 sudo dpkg-reconfigure locales
 sudo apt-get install nginx -y
 sudo systemctl start nginx
