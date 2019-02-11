c272ce35adfe41ac15163c18565865f3af787e42
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 13:57:02 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -1,7 +1,7 @@
 echo "hello"
-sudo apt-get install language-pack-en -y
-update-locale LANG="en_US.utf-8" 
-update-locale LC_ALL="en_US.utf-8"
+export LANGUAGE="en_US.UTF-8"
+echo 'LANGUAGE="en_US.UTF-8"' >> /etc/default/locale
+echo 'LC_ALL="en_US.UTF-8"' >> /etc/default/locale
 locale -a
 locale
 sudo apt-get update -y
