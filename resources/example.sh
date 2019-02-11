6e437fbdda1d81d814b1283897f4ac44535e1531
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 13:53:26 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -1,11 +1,11 @@
 echo "hello"
 sudo apt-get install language-pack-en -y
+update-locale LANG="en_US.utf-8" 
+update-locale LC_ALL="en_US.utf-8"
+locale -a
+locale
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo ufw enable
-sudo ufw allow 8080
-sudo ufw allow 'Nginx HTTP'
-sudo ufw status
 sudo systemctl start nginx
 sudo systemctl status nginx
 echo "nginx installed successfully............"
