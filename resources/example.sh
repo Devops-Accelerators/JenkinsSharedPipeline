ab339d6bc042cfc9505449287e1865ec09786588
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 09:54:57 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -1,6 +1,8 @@
 echo "hello"
 sudo apt-get update -y
 sudo apt-get install nginx -y
+sudo ufw allow 'Nginx HTTP'
+sudo ufw status
 sudo systemctl start nginx
 sudo systemctl status nginx
 echo "nginx installed successfully............"
