cd168254143ae1273801cd7c1baeac63e7d9e1f5
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 22:54:01 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -1,9 +1,9 @@
 export DEBIAN_FRONTEND=noninteractive
 echo "hello"
 sudo apt-get update -y
-sudo locale-gen en_US.UTF-8
-sudo apt-get install language-pack-en -y
-sudo dpkg-reconfigure locales
+#sudo locale-gen en_US.UTF-8
+#sudo apt-get install language-pack-en -y
+#sudo dpkg-reconfigure locales
 sudo apt-get install nginx -y
 sudo systemctl start nginx
 sudo systemctl status nginx
