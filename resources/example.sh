80a87a845650878fcb232c6a1dffcb8e9b509688
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-11 11:24:53 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -3,7 +3,6 @@ sudo locale-gen de_DE.UTF-8 en_US.UTF-8
 sudo apt-get install language-pack-en -y
 sudo apt-get update -y
 sudo apt-get install nginx -y
-sudo apt-get install ufw
 sudo ufw enable
 sudo ufw allow 8080
 sudo ufw allow 'Nginx HTTP'
