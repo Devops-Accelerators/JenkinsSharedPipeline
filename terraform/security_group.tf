d40891b6fcecbd50db08530aa3bc23e0165f0439
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-12 15:16:32 +0530

    Create security_group.tf

---
+++ terraform/security_group.tf
@@ -0,0 +1,26 @@
+resource "aws_security_group" "default" {
+  name = "sas_security_group"
+
+  # Allow all inbound
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Enable ICMP
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
