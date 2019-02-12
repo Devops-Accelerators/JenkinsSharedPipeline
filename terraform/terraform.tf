3dcf53663673bfe8eacc5239847fefaf14a0b17f
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-12 15:19:31 +0530

    Update terraform.tf

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "inst" {
           ami ="${data.aws_ami.image.id}"
           instance_type ="${var.instance_type}"
           key_name="packkey"
+          vpc_security_group_ids = ["${aws_security_group.default.id}"]
         tags{
                 Name="my_instance"
             }
