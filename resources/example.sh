4725ec7272b5ae8ac5b15539b373e04cd42edbb8
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-12 10:46:47 +0530

    Update example.sh

--- resources/example.sh
+++ resources/example.sh
@@ -1,21 +1,13 @@
-export DEBIAN_FRONTEND=noninteractive
 echo "hello"
 sudo apt-get update -y
-#sudo locale-gen en_US.UTF-8
-#sudo apt-get install language-pack-en -y
-#sudo dpkg-reconfigure locales
 sudo locale-gen de_DE.UTF-8 en_US.UTF-8
 sudo apt-get install language-pack-en -y
-#export LANGUAGE=en_US.UTF-8
-#export LC_ALL=en_US.UTF-8
-#export LANG=en_US.UTF-8
-#export LC_TYPE=en_US.UTF-8
 sudo echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 sudo echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 sudo echo "LANG=en_US.UTF-8" > /etc/locale.conf
 locale-gen en_US.UTF-8
 sudo locale-gen
-sudo dpkg-reconfigure locales
+sudo dpkg-reconfigure locale
 sudo locale
 sudo apt-get install nginx -y
 sudo systemctl start nginx
