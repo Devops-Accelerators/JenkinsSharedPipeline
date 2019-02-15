55ddb41e5bdf1bad1cea32f97853cdeb060f5ede
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-15 09:43:03 +0530

    Update security_group.tf

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -4,7 +4,7 @@ resource "aws_security_group" "default" {
   
   ingress {
     from_port   = 0
-    to_port     = 8085
+    to_port     = 65535
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
