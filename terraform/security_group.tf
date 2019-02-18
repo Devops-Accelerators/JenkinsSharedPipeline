db39c8fb9f9620f910954174f995d415c1c78612
soumianisoumya <47141016+soumianisoumya@users.noreply.github.com>
2019-02-18 12:21:17 +0530

    Update security_group.tf

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -1,18 +1,35 @@
 resource "aws_security_group" "default" {
   name = "sas_security_group"
 
-  
+  # SSH access from anywhere
   ingress {
-    from_port   = 0
-    to_port     = 65535
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP access from anywhere
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+   # custom tcp port open
+  ingress {
+    from_port   = 8084
+    to_port     = 8084
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # outbound internet access
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
   }
