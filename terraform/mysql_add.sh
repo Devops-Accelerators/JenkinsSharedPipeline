1f99d844e1b3d22f77e61198357a92a5028618ea
MindtreeDevOps <48236651+MindtreeDevops@users.noreply.github.com>
2019-03-07 10:00:30 +0530

    Update mysql_add.sh

--- terraform/mysql_add.sh
+++ terraform/mysql_add.sh
@@ -2,10 +2,5 @@ sudo docker start helloWorld crudmysql
 sudo docker ps
 ip1=$(sudo docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' crudmysql)
 echo "${ip1}"
-sudo docker exec -it crudmysql bash
-mysql -u root -p Welcome123
-echo "create database `springdb`"
-use springdb
-CREATE TABLE PERSON(id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL, country VARCHAR(30) NOT NULL)
-show table
+
  
