e679a17c0960e071854205aac1f5d8f59b73cfbd
DevOps-Accelerators <sasdevops@gmail.com>
2019-09-17 17:59:48 +0530

    Update packerexec.groovy

--- vars/packerexec.groovy
+++ vars/packerexec.groovy
@@ -1,5 +1,6 @@
 def call(String args){
   echo "path=${args}"
   sh "packer validate ${args}"
+  echo pwd
   sh "packer build -machine-readable -var aws_access_key=\"${AWS_ACCESS_KEY_ID}\"  -var aws_secret_key=\"${AWS_SECRET_ACCESS_KEY}\" ${args}"
   }
